6de314950c057eacf33bc8a1019226d6807bbcd1
Harshanan Saddanathan <hsaddanathan21@gmail.com>
2020-11-25 17:28:54 -0500

    Added preliminary prompt and switch case for prompt.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -11,11 +12,75 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "Buffalobills17.",
-  database: "employee_roster_db"
+  database: "employee_roster_db",
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  connection.end();
+  userPrompt();
 });
+
+function userPrompt() {
+  inquirer
+    .prompt({
+      type: "list",
+      message: "What would you like to do?",
+      name: "userAction",
+      choices: [
+        "View All Employees",
+        "View All Employees by Department",
+        "View All Employees by Manager",
+        "Add Employee",
+        "Add Role",
+        "Add Department",
+        "View All Roles",
+        "View All Departments",
+        "Remove Employee",
+        "Remove Role",
+        "Remove Department",
+        "Exit",
+      ],
+    })
+    .then(function (response) {
+      switch (response.userAction) {
+        case "View All Employees":
+          viewAllEmployees();
+          break;
+        case "View All Employees by Department":
+          employeeDepartmentView();
+          break;
+        case "View All Employees by Manager":
+          employeeManagerView();
+          break;
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        case "Add Department":
+          addDepartment();
+          break;
+        case "View All Roles":
+          viewAllRoles();
+          break;
+        case "View All Departments":
+            viewAllDepartments();
+            break;
+        case "Remove Employee":
+            removeEmployee();
+            break;
+        case "Remove Role":
+            removeRole();
+            break;
+        case "Remove Department":
+            removeDepartment();
+            break;
+        case "Exit":
+            connection.end();
+        
+
+      }
+    });
+}
--- schema.sql
+++ schema.sql
@@ -28,3 +28,24 @@ CREATE TABLE department (
 );
 
 
+INSERT INTO department (name)
+VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
+
+SELECT * FROM department;
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1), ("Sales Person", 80000, 1), ("Lead Engineer", 150000, 2), ("Software Engineer", 120000, 2), ("Lead Accountant", 125000, 3),("Junior Accountant", 90000, 3), ("Legal Team Lead", 250000, 4), ("Lawyer", 190000, 4);
+
+SELECT * FROM role;
+
+INSERT INTO employee (firstName,lastName, roleId,managerId)
+VALUES  ("John","Doe",1,null), ("Mike","Chan",2,1), ("Ashley", "Rodriguez", 3, null), ("Kevin","Tupik",4,3),("Kunal", "Singh", 5, null), ("Malia", "Brown", 6, 5), ("Sarah", "Lourd", 7, null), ("Tom", "Allen", 8, 7);
+
+SELECT * FROM employee;
+
+-- View all Employee info
+SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, name AS Department
+FROM employee 
+LEFT JOIN role ON employee.role_id = role_id 
+LEFT JOIN department ON role.department_id = department_id;
+
